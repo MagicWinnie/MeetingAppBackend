ef359ffcc3085363904fe9b01c45af9f3e560bf2
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-04-06 11:14:54 +0700

    Added force to logging

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
+    force=True,
 )
 
 logger = logging.getLogger(__name__)
